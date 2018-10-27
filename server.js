b8a4efcd37461f8e261616297c19fa11bc876162
unknown <jpalvaro@up.edu.ph>
2018-10-27 17:35:44 +0800

    initial learning

--- server.js
+++ server.js
@@ -15,20 +15,83 @@ MongoClient.connect(databaseUrl, function (err, client) {
 
   database = client.db('food-hunter-poc');
 
-  console.log(`Connection to database successful!`);
-  // const query = {
-  //   shopId: '0001'
-  // }
-  // database.collection('consumables').find(query).toArray((err, result) => {
-  //   if (err) throw err
+  // console.log(`Connection to database successful!`);
+  // // GET DATA
+  // const query1 = {
+  //   _id: '0002'
+  // };
+  // database.collection('consumables').find().toArray((err, result) => {
+  //   if (err) throw err;
 
   //   console.log(result);
-  // })
+  // });
+
   // database.collection('shops').find().toArray((err, result) => {
-  //   if (err) throw err
+  //   if (err) throw err;
 
   //   console.log(result);
-  // })
+  // });
+
+  // // // UPDATE DATA
+  // // const query2 = {
+  // //   _id: '0002'
+  // // };
+
+  // // const newval2 = {
+  // //   $set: {
+  // //     type: 'Food',
+  // //     price: 30,
+  // //     name: 'Dalandan',
+  // //     amount: '300mL'
+  // //   }
+  // // };
+
+
+  // // database.collection('consumables').updateOne(query2, newval2, (err, result) => {
+  // //   if (err) throw err;
+
+  // //   console.log('Updated successfully!');
+  // // });
+
+  // // DELETE DATA
+  // const query3 = {
+  //   _id: '0003'
+  // };
+
+  // database.collection('shops').deleteOne(query3, (err, result) => {
+  //   if (err) throw err;
+
+  //   console.log('Deleted successfully!');
+  // });
+
+  // // ADD DATA
+  // const addval1 = {
+  //   _id: '0003',
+  //   name: 'Tita Carmen\'s',
+  //   type: 'Stall',
+  //   estimatedAddress: 'Magsaysay St., Diliman, Quezon City, Metro Manila',
+  //   contact: {person: 'Carmen Olivares', number: '09921334412'},
+  //   operation: {hour: null, days: null},
+  //   consumables_id: ['0002', '0003']
+  // };
+
+  // database.collection('shops').insertOne(addval1, (err, result) => {
+  //   if (err) throw err;
+
+  //   console.log('Inserted successfully!!');
+  // });
+
+  // // database.collection('shops').find().toArray((err, result) => {
+  // //   if (err) throw err;
+
+  // //   console.log(result);
+  // // });
+
+  // database.collection('shops').find().toArray((err, result) => {
+  //   if (err) throw err;
+
+  //   console.log(result);
+  // });
 });
 
 // configure  server
@@ -43,31 +106,31 @@ api.use(function (req, res, next) {
 
 
 // configure routes for api
-api.get('/shops', (requst, respose) => {
+api.get('/api/shops', (requst, respose) => {
   database.collection('shops').find().toArray((err, result) => {
-    if (err) throw err
+    if (err) throw err;
 
     respose.send(result);
   })
 });
 
-api.get('/shops/:shopId', (requst, respose) => {
+api.get('/api/shops/:shopId', (requst, respose) => {
   const query = {
     shopId: requst.params.shopId
   }
   database.collection('shops').find(query).toArray((err, result) => {
-    if (err) throw err
+    if (err) throw err;
 
     respose.send(result);
   })
 });
 
-api.get('/shops/:shopId/consumables', (requst, respose) => {
+api.get('/api/shops/:shopId/consumables', (requst, respose) => {
   const query = {
     shopId: requst.params.shopId
   }
   database.collection('consumables').find(query).toArray((err, result) => {
-    if (err) throw err
+    if (err) throw err;
 
     respose.send(result);
   })
