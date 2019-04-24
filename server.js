302a30a7a7f6a9a07671ec84ee064cb5fab09f71
justinealvaro15 <jpalvaro@up.edu.ph>
2019-04-24 22:39:26 +0800

    Added feature: filter all shops

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ api.get('/api/shops', (request, response) => {
   })
 });
 
-//// GET SHOPS DISPLAY
+//// GET ALL SHOPS DISPLAY
 api.get('/api/shops/display', (request, response) => {
   const query = {
     active: true
@@ -269,7 +269,16 @@ api.post('/api/shops/:shopId/food', (request, response) => {
   })
 });
 
-//// GET REVIEWS
+//// GET ALL REVIEWS
+api.get('/api/reviews', (request, response) => {
+  database.collection('reviews').find().toArray((err, result) => {
+    if (err) throw err;
+
+    response.send(result);
+  })
+});
+
+//// GET REVIEWS OF A SHOP
 api.get('/api/reviews/:shopId', (request, response) => {
   const query = {
     fe_id: request.params.shopId
@@ -397,14 +406,15 @@ api.post('/api/admin', (request,response)=> {
       if (err) throw err;
   });
 });
-//DELETE ADMIN
+
+//// DELETE ADMIN
 api.delete('/api/admin/:user_id', (request,response)=> {
   database.collection('admin').deleteOne(
     {user_id: request.params.user_id}
   );
 });
 
-//ADD USER
+//// ADD USER
 api.post('/api/users', (request,response)=> {
   const newUser = {
     user_id: request.body.user_id,
@@ -448,7 +458,7 @@ api.post('/api/users', (request,response)=> {
   });
 });
 
-//EDIT USER
+//// EDIT USER
   //active to inactive and vice versa
 api.put('/api/users/:user_id', bodyParser.json(), (request,response)=> {
   const query = {
